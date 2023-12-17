3b71dff660be575606590f28d0d57794074381ff
takker99 <37929109+takker99@users.noreply.github.com>
2023-12-17 11:56:04 +0900

    :bug: base64 encodingがうまくいかなかったので、escapeだけしてData URLに入れる

--- compilers/svg.ts
+++ compilers/svg.ts
@@ -1,11 +1,10 @@
 import { content } from "../codeBlock.ts";
 import { Compile } from "../compile.ts";
-import { encodeBase64Url } from "../deps/base64url.ts";
 
 export const previewSVG: Compile = (compileInit) => {
   if (!("after" in compileInit)) return undefined;
-  const url = `data:image/svg+xml;base64,${
-    encodeBase64Url(content(compileInit.after))
+  const url = `data:image/svg+xml;charset=utf8,${
+    encodeURIComponent(content(compileInit.after))
   }`;
   const img = document.createElement("img");
   img.src = url;
--- mod.ts
+++ mod.ts
@@ -2,4 +2,3 @@ export * from "./setup.ts";
 export * from "./codeBlock.ts";
 export * from "./compile.ts";
 export * from "./compilers/svg.ts";
-export * from "./compilers/plantUML/mod.ts";
