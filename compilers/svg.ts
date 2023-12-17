da9cb0f75ce5cc0e9b6f87b48c4299997044c8e7
takker99 <37929109+takker99@users.noreply.github.com>
2023-12-17 12:13:19 +0900

    :sparkle: SVGの構文エラーを表示する
    DOMParserで検知できないものは諦める

--- compilers/svg.ts
+++ compilers/svg.ts
@@ -3,11 +3,21 @@ import { Compile } from "../compile.ts";
 
 export const previewSVG: Compile = (compileInit) => {
   if (!("after" in compileInit)) return undefined;
-  const url = `data:image/svg+xml;charset=utf8,${
-    encodeURIComponent(content(compileInit.after))
-  }`;
+  const svg = content(compileInit.after);
+  const dom = new DOMParser().parseFromString(svg, "image/svg+xml");
+  const error = dom.querySelector("parsererror");
+  // if failed to parse
+  if (error) {
+    const pre = document.createElement("pre");
+    const code = document.createElement("code");
+    code.innerText = error.innerHTML;
+    pre.append(code);
+    compileInit.render(pre);
+    return;
+  }
+
+  const url = `data:image/svg+xml;charset=utf8,${encodeURIComponent(svg)}`;
   const img = document.createElement("img");
   img.src = url;
   compileInit.render(img);
-  return undefined;
 };
