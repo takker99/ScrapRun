c953f45373208a91a84c680119de083d73ef9292
takker99 <37929109+takker99@users.noreply.github.com>
2023-12-17 13:32:25 +0900

    :bug: Cannot fetch DataURL of wasm
    `fetch`せず、blob形式で取り込めるversionに落とした

--- compilers/plantUML/toPlantumlURL.ts
+++ compilers/plantUML/toPlantumlURL.ts
@@ -1,12 +1,12 @@
-import { deflate, initBundledOnce, Memory } from "../../deps/denoflate.ts";
+import { deflate, initSyncBundledOnce } from "../../deps/denoflate.ts";
 import { encode64, textToBuffer } from "./encode.ts";
 
-await initBundledOnce();
 export const toPlantUML = (
   uml: string,
   type: "svg" | "png" | "dsvg" | "dpng" = "svg",
 ) => {
+  initSyncBundledOnce();
   return `https://www.plantuml.com/plantuml/${type}/${
-    encode64(deflate(new Memory(textToBuffer(uml)), 9).bytes)
+    encode64(deflate(textToBuffer(uml), 9))
   }`;
 };
--- deps/denoflate.ts
+++ deps/denoflate.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/foras@v2.1.4/src/deno/mod.ts";
+export * from "https://deno.land/x/foras@2.0.8/src/deno/mod.ts#=";
