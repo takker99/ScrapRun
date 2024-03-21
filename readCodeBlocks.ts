3e11566600096390d05bb2142bf6d22216f9da4b
takker99 <37929109+takker99@users.noreply.github.com>
2024-03-21 17:30:17 +0900

    build: Update Deno dependencies
    scrapbox-parserの破壊的変更に対応

--- readCodeBlocks.ts
+++ readCodeBlocks.ts
@@ -1,8 +1,4 @@
-import {
-  convertToBlock,
-  packRows,
-  parseToRows,
-} from "./deps/scrapbox-parser.ts";
+import { parse } from "./deps/scrapbox-parser.ts";
 import { Blocks } from "./codeBlock.ts";
 
 export const readCodeBlocks = <Line extends { id: string; text: string }>(
@@ -10,31 +6,30 @@ export const readCodeBlocks = <Line extends { id: string; text: string }>(
 ): Map<string, Blocks<Line>> => {
   const codeBlocks = new Map<string, Line[][]>();
   if (lines.length === 0) return codeBlocks;
-  const packs = packRows(
-    parseToRows(lines.map((line) => line.text).join("\n")),
+  const packs = parse(
+    lines.map((line) => line.text).join("\n"),
     { hasTitle: true },
   );
 
   /** 現在読んでいる`pack.rows`の行番号 */
   let counter = 0;
 
-  for (const pack of packs) {
-    switch (pack.type) {
+  for (const block of packs) {
+    switch (block.type) {
       case "title":
       case "line":
         counter++;
         break;
       case "table":
-        counter += pack.rows.length;
+        counter += block.cells.length + 1;
         break;
       case "codeBlock": {
-        const block = convertToBlock(pack);
-        if (block.type !== "codeBlock") throw Error("Must be a codeblock");
         const blocks = codeBlocks.get(block.fileName) ??
           [];
-        blocks.push(lines.slice(counter, counter + pack.rows.length));
+        const lineCount = block.content.split("\n").length + 1;
+        blocks.push(lines.slice(counter, counter + lineCount));
         codeBlocks.set(block.fileName, blocks);
-        counter += pack.rows.length;
+        counter += lineCount;
         break;
       }
     }
