4578f48c8e987f4a112b91fef6ef147752e20b3d
GitHub <noreply@github.com>
2024-05-03 00:37:41 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox.ts
+++ deps/scrapbox.ts
@@ -1,2 +1,2 @@
-export * from "https://raw.githubusercontent.com/scrapbox-jp/types/0.6.1/rest.ts";
-export type { Scrapbox } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.6.1/userscript.ts";
+export * from "https://raw.githubusercontent.com/scrapbox-jp/types/0.7.0/rest.ts";
+export type { Scrapbox } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.7.0/userscript.ts";
