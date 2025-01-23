55a29d7bead52c456b6711801ae721303f074b12
GitHub <noreply@github.com>
2025-01-23 00:44:29 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox-std-browser.ts
+++ deps/scrapbox-std-browser.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.4/browser/dom/mod.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.5/browser/dom/mod.ts";
